920c0691f24bd898a25339cb3dc1d6a05131ed34
giangndj-hlg <giang.ndj.hlg@gmail.com>
2024-05-08 23:28:54 +0700

    first commit

---
+++ src/main/java/com/mshop/apigateway/MonitorShopApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mshop.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorShopApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorShopApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mshop/apigateway/config/Config.java
@@ -0,0 +1,27 @@
+package com.mshop.apigateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(corsConfigurationSource);
+    }
+
+}
